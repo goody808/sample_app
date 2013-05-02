94498e3438cd6b1a644d13f2f3095750c19517e4
goody808 <joshua.gudjohnsen@gmail.com>
2013-05-01 21:15:30 -0400

    Update user mdel

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
- has_secure_password
+  has_secure_password
 
   before_save { |user| user.email = user.email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
