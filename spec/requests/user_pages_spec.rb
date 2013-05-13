5d9c8fdb6d0e672cf0328511ab86a70b36284ffe
goody808 <joshua.gudjohnsen@gmail.com>
2013-05-13 17:13:30 -0400

    finished final part of chapter 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -18,6 +18,14 @@ describe "Sign Up Page" do
 	  it "should not add a user to the db" do 	
 	  	expect {click_button submit}.not_to  change(User, :count) 
 	  end
+
+	  describe "after submision" do 
+	  	before { click_button submit }
+	  	
+	  	it { should have_selector('title', text: 'Sign Up') }
+	  	it { should have_content('errors') }
+	  	it { should_not have_content('digest') }
+	  end
 	end 
 
 	describe "valid signup" do 
@@ -32,19 +40,6 @@ describe "Sign Up Page" do
 			expect {click_button submit}.to change(User, :count).by(1)
 		end
 	end
-
-	# describe "invalid email address" do 
-	# 	before do 
-	# 		fill_in "Name", 								 with: "Example User"
-	# 		fill_in "Email", 								 with: "exampleuser.com"
-	# 		fill_in "Password", 						 with: "foobar"
-	# 		fill_in "Password Confirmation", with: "foobar"
-	# 	end
-		
-	# 	it "should have error message on page 'Email is Invalid" do
-	# 	click_button submit page.should have_content(text: "Email is invalid")
-	# 	end
-	# end
 end 
   
 describe "profile page" do
@@ -53,5 +48,6 @@ describe "profile page" do
 
 	  it { should have_selector('h1',    text: user.name) }
 	  # it { should have_selector('title', text: user.name) }
+	  
 	end
 end
